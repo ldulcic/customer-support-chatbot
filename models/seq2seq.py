3c7dcb8c971007c66da07c33426926c7ae801bdb
Luka Dulčić <luka.dulcic@fer.hr>
2018-05-17 18:25:34 +0200

    Bahdanau attention decoder.

--- models/seq2seq.py
+++ models/seq2seq.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import random
 
 
@@ -31,6 +32,87 @@ class Encoder(nn.Module):
         embedded = self.embed(input)
         outputs, h_n = self.rnn(embedded, h_0)
         return outputs, h_n
+    
+    
+class BahdanauAttnDecoder(nn.Module):
+    """
+    Bahdanau attention decoder for seq2seq models.
+
+    :param vocab_size: Size of vocabulary over which we operate.
+    :param embed_size: Dimensionality of word embeddings.
+    :param hidden_size: Dimensionality of RNN hidden representation.
+    :param encoder_hidden_size: Dimensionality of encoder hidden representation (important for calculating attention 
+                                context)
+    :param attn_hidden_size: Attention layer hidden size
+    :param padding_idx: Index of pad token in vocabulary. 
+    :param num_layers: Number of layers in RNN. Default: 1.
+
+    Inputs: input, last_hidden
+        - **input** (batch): Input word.
+        - **last_hidden** (num_layers, batch, hidden_size): Last RNN hidden state, in first step
+        this will be last hidden state of encoder and in subsequent steps it will be decoder hidden state from
+        previous step.
+        - **encoder_outputs** (seq_len, batch, encoder_hidden_size): Last encoder layer outputs for every timestamp.
+
+    Outputs: output, hidden
+        - **output** (batch, vocab_size): Prediction of next word in output sequence, logarithm of probability
+        distribution over vocabulary (log_softmax).
+        - **hidden** (num_layers, batch, hidden_size): New RNN hidden state.
+    """
+    def __init__(self, vocab_size, embed_size, hidden_size, encoder_hidden_size, attn_hidden_size, padding_idx, num_layers=1):
+        super(BahdanauAttnDecoder, self).__init__()
+        self.embed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size, padding_idx=padding_idx)
+        self.rnn = nn.GRU(input_size=embed_size + encoder_hidden_size, hidden_size=hidden_size, num_layers=num_layers)
+        self.out = nn.Linear(in_features=hidden_size, out_features=vocab_size)
+
+        # attention params
+        self.W = nn.Linear(in_features=hidden_size + encoder_hidden_size, out_features=attn_hidden_size)
+        self.v = nn.Linear(in_features=attn_hidden_size, out_features=1)
+
+    def forward(self, input, last_hidden, encoder_outputs):
+        embedded = self.embed(input)
+        
+        # prepare rnn input
+        context = self.attn_context(last_hidden[-1], encoder_outputs)
+        rnn_input = torch.cat([embedded, context], dim=1)
+        rnn_input = rnn_input.unsqueeze(0)  # (batch, embed + enc_h) -> (1, batch, embed + enc_h)
+        
+        # calculate decoder output
+        _, hidden = self.rnn(rnn_input, last_hidden)
+        output = self.out(hidden[-1])  # hidden[-1] - hidden output of last layer
+        
+        return output, hidden
+
+    def print_dim(self, name, tensor):
+        print("%s -> %s" % (name, tensor.size()))
+        
+    def attn_context(self, hidden, encoder_outputs):
+        """
+        Generates attention context vector given last decoder hidden output and all encoder outputs.
+        
+        :param hidden (batch, hidden_size): Last decoder layer hidden output. 
+        :param encoder_outputs (seq_len, batch, encoder_hidden_size): Last encoder layer outputs for every timestamp.
+        :return: Attention context vector (batch, encoder_hidden_size)
+        """
+        energies = self.attn_energies(hidden, encoder_outputs)
+        scores = F.softmax(energies, dim=0).unsqueeze(2) # (batch, seq_len, 1)
+        enc_out = encoder_outputs.transpose(0, 1).transpose(1, 2)  # (seq_len, batch, enc_h) -> (batch, enc_h, seq_len)
+        context = torch.bmm(enc_out, scores)  # (batch, enc_h, 1)
+        return context.squeeze(2)
+
+    def attn_energies(self, hidden, encoder_outputs):
+        """
+        Generates attention energies for current decoder position.
+
+        :param hidden (batch, hidden_size): Last decoder layer hidden output.
+        :param encoder_outputs (seq_len, batch, encoder_hidden_size): Last encoder layer outputs for every timestamp.
+        :return: Attention energies (batch, seq_len)
+        """
+        seq_len = encoder_outputs.size(0)
+        h = hidden.expand(seq_len, -1, -1)  # (batch, hidden_size) -> (seq_len, batch, hidden_size)
+        energies = self.W(torch.cat([encoder_outputs, h], dim=2))  # (seq_len, batch, attn_h)
+        energies = self.v(F.tanh(energies))  # (seq_len, batch, 1)
+        return energies.squeeze(2).transpose(0, 1)
 
 
 class Decoder(nn.Module):
@@ -98,7 +180,7 @@ class Seq2Seq(nn.Module):
         hidden = h_n  # output of all encoder layers for t=seq_len
         input_word = trg[0]  # sos for whole batch
         for t in range(trg_seq_len):
-            output, hidden = self.decoder(input_word, hidden)
+            output, hidden = self.decoder(input_word, hidden, encoder_outputs)
             outputs[t] = output
 
             teacher_forcing = random.random() < teacher_forcing_ratio
@@ -117,7 +199,7 @@ class Seq2Seq(nn.Module):
         hidden = h_n
 
         out = []
-        while len(out) < 10:
+        while len(out) < 20:
             output, hidden = self.decoder(input_word, hidden)
             _, argmax = output.squeeze(0).data.max(dim=0)
             out.append(argmax.item())
