c2e4062998fe4b940cb7a8d83b3390667c11d025
Luka Dulčić <luka.dulcic@fer.hr>
2018-06-06 11:43:01 +0200

    Fix for loading model which was trained on multiple GPUs.

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 from models.seq2seq.encoder import encoder_factory
 from models.seq2seq.decoder import decoder_factory
 from models.seq2seq.model import Seq2SeqTrain, Seq2SeqPredict
+from collections import OrderedDict
 
 
 def train_model_factory(args, metadata):
@@ -12,5 +13,21 @@ def train_model_factory(args, metadata):
 
 def predict_model_factory(args, metadata, model_path, field):
     train_model = train_model_factory(args, metadata)
-    train_model.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage))
+    train_model.load_state_dict(get_state_dict(args, model_path))
     return Seq2SeqPredict(train_model.encoder, train_model.decoder, field)
+
+
+def get_state_dict(args, model_path):
+    # load state dict and map it to current storage (CPU or GPU)
+    state_dict = torch.load(model_path, map_location=lambda storage, loc: storage)
+
+    # if model was trained with DataParallel (on multiple GPUs) remove "module." at the beginning of every key in state
+    # dict (so we can load model on 1 GPU or on CPU for inference)
+    if args.cuda and args.multi_gpu:
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            key = k[7:]  # remove "module."
+            new_state_dict[key] = v
+        return new_state_dict
+
+    return state_dict
